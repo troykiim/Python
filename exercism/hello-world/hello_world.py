779f9d4022dcd3e93ac63890d42314fa91c92a9b
troykiim <troykiim@gmail.com>
2017-11-15 16:39:32 -0500

    Python Learning Materials

---
+++ exercism/hello-world/hello_world.py
@@ -0,0 +1,4 @@
+def hello(name='World'):
+    "hello says hello to you!"
+    return 'Hello, {}!'.format(name)
+    
---
+++ pyPrograms/http_proxy.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+print("http_proxy=http://192.168.2.1:3128; export http_proxy")
+
